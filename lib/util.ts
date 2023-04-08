08ca4a59a5c7d1063e8fe0935a7f9350728bb1b3
Josh Elias <code.5jzcm@simplelogin.com>
2023-04-07 21:24:38 -0400

    Add camelcase Zod utility

---
+++ lib/util.ts
@@ -0,0 +1,6 @@
+import { z, ZodEffects } from 'zod';import camelcaseKeys from 'camelcase-keys';
+import { CamelCasedPropertiesDeep } from 'type-fest';
+
+export const zodToCamelCase = 
+    <T extends z.ZodTypeAny>(zod: T): ZodEffects<z.ZodTypeAny, CamelCasedPropertiesDeep<T['_output']>> => 
+        zod.transform((val) => camelcaseKeys(val) as CamelCasedPropertiesDeep<T>)

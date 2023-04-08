493d665d140f36016e68e15cafbe73e27a65d55d
Josh Elias <code.5jzcm@simplelogin.com>
2023-04-08 01:39:39 -0400

    Add more types from Zod schemas

--- lib/types.ts
+++ lib/types.ts
@@ -4,11 +4,21 @@ import {
     EndDateInputSchema,
     NumberOfLandingsInputSchema,
     TotalFlyingHoursInputSchema,
-    TripInputSchema
+    FindAvailableAircraftInputSchema
 } from './schema';
 
 export type StartDateInput = z.infer<typeof StartDateInputSchema>;
 export type EndDateInput = z.infer<typeof EndDateInputSchema>;
 export type NumberOfLandingsInput = z.infer<typeof NumberOfLandingsInputSchema>;
 export type TotalFlyingHoursInput = z.infer<typeof TotalFlyingHoursInputSchema>;
-export type TripInput = z.infer<typeof TripInputSchema>;
+export type FindAvailableAircraftInput = z.infer<typeof FindAvailableAircraftInputSchema>;
+
+
+import { 
+    AircraftOutputCamelSchema,
+    MaintenanceScheduleOutputCamelSchema,
+    TripOutputCamelSchema,
+} from './api';
+export type TripOutput = z.infer<typeof TripOutputCamelSchema>;
+export type AircraftOutput = z.infer<typeof AircraftOutputCamelSchema>;
+export type MaintenanceScheduleOutput = z.infer<typeof MaintenanceScheduleOutputCamelSchema>;

340a3c6a422ea0c8d31fb2d6d334870d98c4a06c
Josh Elias <code.5jzcm@simplelogin.com>
2023-04-07 21:39:02 -0400

    Add missing resource endpoints to the API client

--- lib/api.ts
+++ lib/api.ts
@@ -3,6 +3,39 @@ import { Zodios, makeErrors } from '@zodios/core';
 import { BASE_API_URL } from './constants';
 
 
+export const AircraftOutputSchema = z.object({
+    id: z.number(),
+    tail_number: z.string(),
+    current_hobbs: z.number(),
+    current_landings: z.number(),
+    last_maintenance_date: z.string(),
+});
+
+export const MaintenanceTypeOutputSchema = z.object({
+    id: z.number(),
+    description: z.string(),
+    hobbs_interval: z.number(),
+    landings_interval: z.number(),
+    calendar_days_interval: z.number(),
+});
+
+export const MaintenanceEventOutputSchema = z.object({
+    id: z.number(),
+    aircraft_id: z.number(),
+    maintenance_type_id: z.number(),
+    next_due_hobbs: z.number(),
+    next_due_landings: z.number(),
+    next_due_date: z.string(),
+});
+
+export const MaintenanceScheduleOutputSchema = z.object({
+    id: z.number(),
+    aircraft_id: z.number(),
+    maintenance_type_id: z.number(),
+    start_date: z.string(),
+    end_date: z.string(),
+});
+
 export const TripOutputSchema = z.object({
     id: z.number(),
     aircraft_id: z.number(),
@@ -11,7 +44,7 @@ export const TripOutputSchema = z.object({
     flying_time: z.number(),
     landing_count: z.number()
 });
-export type TripOutput = z.infer<typeof TripOutputSchema>;
+
 
 export const errors = makeErrors([
     {
@@ -26,7 +59,40 @@ export const errors = makeErrors([
     },
 ]);
 
+
 export const api = new Zodios(BASE_API_URL, [
+    {
+        method: "get",
+        path: "/aircraft",
+        alias: "getAircraft",
+        description: "Get all aricraft",
+        response: z.array(AircraftOutputSchema),
+        errors: errors,
+    },
+    {
+        method: "get",
+        path: "/maintenance_type",
+        alias: "getMaintenanceTypes",
+        description: "Get all maintenance types",
+        response: z.array(MaintenanceTypeOutputSchema),
+        errors: errors,
+    },
+    {
+        method: "get",
+        path: "/maintenance_event",
+        alias: "getMaintenanceEvents",
+        description: "Get all maintenance events",
+        response: z.array(MaintenanceEventOutputSchema),
+        errors: errors,
+    },
+    {
+        method: "get",
+        path: "/maintenance_schedule",
+        alias: "getMaintenanceSchedules",
+        description: "Get all maintenance schedules",
+        response: z.array(MaintenanceScheduleOutputSchema),
+        errors: errors,
+    },
     {
         method: "get",
         path: "/trip",
